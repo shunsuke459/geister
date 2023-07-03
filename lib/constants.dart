bffcd57bc953a9509bebccc847f44f551f1f1c4e
shunsuke459 <shunsuke459@gmail.com>
2023-07-03 22:35:34 +0900

    add: Constantsクラス実装

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  Constants._();
+
+  static const arrowTop = Icon(
+    Icons.arrow_upward_rounded,
+    color: Colors.black,
+  );
+  static const arrowDown = Icon(
+    Icons.arrow_downward_rounded,
+    color: Colors.black,
+  );
+}
