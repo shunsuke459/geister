4595c78b503efe3b9fcff137cf41d3a63f1c06be
shunsuke459 <shunsuke459@gmail.com>
2023-07-04 13:33:54 +0900

    add: firebase接続の下準備

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:geister/firebase_options.dart';
 import 'package:geister/page/battle/battle_page.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+void main() async {
+  await initialize();
+
   runApp(
     const ProviderScope(
       child: MyApp(),
     ),
+  );
+}
+
+Future<void> initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 }
 
